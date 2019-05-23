53a6b2f68137c95679d1f62100e3939895e566b7
D-Flime <33832158+D-Flime@users.noreply.github.com>
2019-05-23 21:28:58 +0400

    Update v1.2

--- js/PageManager.js
+++ js/PageManager.js
@@ -18,57 +18,67 @@ class PageManager {
       this.userProfileCloseBtn.onclick = () => this.hideUserProfile();
    }
 
-   showUserProfile(user, avatarUrl, userName) {
-      this.userProfileElement.setAttribute('style', '');
-      this.userProfileBGElement.setAttribute('style', '');
-      //set user name
-      document.querySelector('.user_profile_name').innerHTML = userName;
-      // set user avatar
-      document.querySelector('.user_profile_avatar img').src = avatarUrl;
-      // set user location
-      document.querySelector('.user_profile_location').innerHTML = `${user.location.state}, ${user.location.city}, ${user.location.street}</br>
-                                                                    Postcode: ${user.location.postcode}`;
-      // set user e-mail
-      document.querySelector('.user_profile_email').innerHTML = user.email;
-      // set user phone number
-      document.querySelector('.user_profile_phone').innerHTML = user.phone;                                             
+   showUserProfile(user, userName) {
+      if (user && userName){
+         this.userProfileElement.setAttribute('style', '');
+         this.userProfileBGElement.setAttribute('style', '');
+         //set user name
+         document.querySelector('.user_profile_name').innerHTML = userName;
+         // set user avatar
+         document.querySelector('.user_profile_avatar img').src = user.picture.large;
+         // set user location
+         document.querySelector('.user_profile_location').innerHTML = `${user.location.state}, ${user.location.city}, ${user.location.street}</br>
+                                                                     Postcode: ${user.location.postcode}`;
+         // set user e-mail
+         document.querySelector('.user_profile_email').innerHTML = user.email;
+         // set user phone number
+         document.querySelector('.user_profile_phone').innerHTML = user.phone;
+      }                                             
    }
 
    hideUserProfile() {
       this.userProfileElement.setAttribute('style', 'display: none');
       this.userProfileBGElement.setAttribute('style', 'display: none');
+      // remove user avatar
+      document.querySelector('.user_profile_avatar img').src = '';
    }
 
    addUserBlock(user) {
-      let avatarUrl = user.picture.medium, userName = `${user.name.title} ${user.name.first} ${user.name.last}`;
-      // create user block
-      const userBlockDiv = document.createElement('div');
-      userBlockDiv.className = 'user_block';
-      this.userlistElement.appendChild(userBlockDiv);
-      // add avatar block
-      const avatarDiv = document.createElement('div');
-      avatarDiv.className = 'user_avatar';
-      userBlockDiv.appendChild(avatarDiv);
-      // add avatar image
-      const avatarIMG = document.createElement('img');
-      avatarIMG.src = avatarUrl;
-      avatarDiv.appendChild(avatarIMG);
-      // add content to user block (name)
-      const userContentDiv = document.createElement('div');
-      userContentDiv.className = 'user_block_content';
-      userContentDiv.innerHTML = userName;
-      userBlockDiv.appendChild(userContentDiv);
-      userBlockDiv.onclick = () => this.showUserProfile(user, avatarUrl, userName);
-   }
-
-   clearUserlist(userlist) {
-      this.userlistElement.innerHTML = '';
+      if (user instanceof Object){
+         let avatarUrl = user.picture.medium, userName = `${user.name.title} ${user.name.first} ${user.name.last}`;
+         // create user block
+         const userBlockDiv = document.createElement('div');
+         userBlockDiv.className = 'user_block';
+         this.userlistElement.appendChild(userBlockDiv);
+         // add avatar block
+         const avatarDiv = document.createElement('div');
+         avatarDiv.className = 'user_avatar';
+         userBlockDiv.appendChild(avatarDiv);
+         // add avatar image
+         const avatarIMG = document.createElement('img');
+         avatarIMG.src = avatarUrl;
+         avatarDiv.appendChild(avatarIMG);
+         // add content to user block (name)
+         const userContentDiv = document.createElement('div');
+         userContentDiv.className = 'user_block_content';
+         userContentDiv.innerHTML = userName;
+         userBlockDiv.appendChild(userContentDiv);
+         userBlockDiv.onclick = () => this.showUserProfile(user, userName);
+      }
    }
 
    createUserlist(userlist) {
+      var letter = '';
       if (userlist instanceof Object) {
          for (let key in userlist) {
-            //this.addUserBlock(userlist[key].picture.medium, `${userlist[key].name.title} ${userlist[key].name.first} ${userlist[key].name.last}`);
+            if(userlist[key].name.first[0] !== letter){
+               letter = userlist[key].name.first[0];
+               // add abc letter line into sorted userlist
+               let div = document.createElement('div');
+               div.innerHTML = letter;
+               div.className = 'abc_block';
+               this.userlistElement.appendChild(div);
+            }
             this.addUserBlock(userlist[key]);
          }
       }
@@ -106,13 +116,13 @@ class PageManager {
          this.sortUserlistByABC(userlist);
          this.sortState = 'down';
          this.listsorterElement.innerHTML = '▼ СОРТИРОВАТЬ';
-         this.clearUserlist(userlist);
+         this.userlistElement.innerHTML = '';
          this.createUserlist(userlist);
       } else if (this.sortState === 'down'){
          this.sortUserlistReversed(userlist);
          this.sortState = 'up';
          this.listsorterElement.innerHTML = '▲ СОРТИРОВАТЬ';
-         this.clearUserlist(userlist);
+         this.userlistElement.innerHTML = '';
          this.createUserlist(userlist);
       } else {
          return false;
--- js/main.js
+++ js/main.js
@@ -7,11 +7,8 @@ window.onload = () => {
 
     async function init() {
         data = await router.getUsersData();
-        console.log(data);
         pageManager.userlist = data.results;
-        pageManager.sortUserlistByABC(data.results);
         pageManager.sortUserlist(data.results);
-        pageManager.createUserlist(data.results);
     }
     init();
 
